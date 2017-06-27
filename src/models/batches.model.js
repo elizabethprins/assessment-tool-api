ea2b5b6db21751b59ad44a9f053b4a4e7bf31985
elizabethprins <elizabethprins@gmail.com>
2017-06-27 11:09:32 +0200

    initial commit

---
+++ src/models/batches.model.js
@@ -0,0 +1,30 @@
+// batches-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+
+  const assessmentSchema = new Schema ({
+  date: { type: Date },
+  colourCode: { type: Number },
+  remarks: { type: String },
+  });
+
+  const studentSchema = new schema({
+    assessments: [assessmentSchema],
+    firstName: { type: String },
+    lastName: { type: String },
+    imageURL: { type: String },
+  });
+
+  const batches = new schema({
+    students: [studentSchema],
+    batchNumber:{ type: Number },
+    startDate: { type: Date },
+    endDate: { type: Date },
+  });
+
+  return mongooseClient.model('batches', batches);
+};
