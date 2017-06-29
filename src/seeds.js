6fb035afe34c0cdf79b91eb70cfaf49a494b378c
elizabethprins <elizabethprins@gmail.com>
2017-06-29 23:15:46 +0200

    some extra seed data

--- src/seeds.js
+++ src/seeds.js
@@ -17,11 +17,27 @@ const batches = [
     students: [
       { firstName: 'Aaklo', lastName: 'Stuur', imageURL: 'https://i.imgur.com/NUyttbnh.jpg', assessments: [
         { date: '02-01-2017', colourCode: 1, remarks: 'Wow, I am SO impressed. My work here is done.'},
-        { date: '03-01-2017', colourCode: 2, remarks: '...Or not, whatevs.'}
+        { date: '03-01-2017', colourCode: 2, remarks: 'Knows all the words to Rappers Delight and not afraid to prove it.'}
       ]},
       { firstName: 'Garner', lastName: 'Kinkle', imageURL: 'https://goo.gl/DPyPYh', assessments: [
         { date: '02-01-2017', colourCode: 3, remarks: 'I wish that salt bae guy would come by and just sprinkle some on this person.'},
         { date: '03-01-2017', colourCode: 3, remarks: 'THE STRUGGLE IS REAL'}
+      ]},
+      { firstName: 'Wiers', lastName: 'Brankele', imageURL: 'https://goo.gl/Dc6x4B', assessments: [
+        { date: '06-03-2017', colourCode: 2, remarks: 'Keeps calling me Dad.'},
+        { date: '07-03-2017', colourCode: 1, remarks: 'There is a reason for everything.'}
+      ]},
+      { firstName: 'Vog', lastName: 'Vogon', imageURL: 'https://goo.gl/RTt611', assessments: [
+        { date: '06-03-2017', colourCode: 1, remarks: 'Do not let this guy read you his poetry.'},
+        { date: '07-03-2017', colourCode: 3, remarks: 'Honestly this mark is only because of his awful poetry.'}
+      ]},
+      { firstName: 'Barko', lastName: 'Wilk', imageURL: 'https://i.imgur.com/NUyttbnh.jpg', assessments: [
+        { date: '02-01-2017', colourCode: 3, remarks: 'Keep an eye on this one!'},
+        { date: '03-01-2017', colourCode: 1, remarks: 'Wow, I am SO impressed. My work here is done.'}
+      ]},
+      { firstName: 'Smeini', lastName: 'Luft', imageURL: 'https://i.imgur.com/NUyttbnh.jpg', assessments: [
+        { date: '02-01-2017', colourCode: 1, remarks: 'Wow, I am SO impressed. My work here is done.'},
+        { date: '03-01-2017', colourCode: 1, remarks: '..Or not, whatevs.'}
       ]}
     ]
   },
@@ -32,11 +48,27 @@ const batches = [
     students: [
       { firstName: 'Brankele', lastName: 'Wierd', imageURL: 'https://i.imgur.com/NUyttbnh.jpg', assessments: [
         { date: '06-03-2017', colourCode: 2, remarks: 'Keeps calling me Dad.'},
-        { date: '07-03-2017', colourCode: 1, remarks: 'Seriously, what should I do?'}
+        { date: '07-03-2017', colourCode: 1, remarks: 'There is a reason for everything.'}
       ]},
       { firstName: 'Vog', lastName: 'Vogon', imageURL: 'https://goo.gl/DPyPYh', assessments: [
         { date: '06-03-2017', colourCode: 1, remarks: 'Do not let this guy read you his poetry.'},
         { date: '07-03-2017', colourCode: 3, remarks: 'Honestly this mark is only because of his awful poetry.'}
+      ]},
+      { firstName: 'Aaklo', lastName: 'Stuur', imageURL: 'https://i.imgur.com/NUyttbnh.jpg', assessments: [
+        { date: '02-01-2017', colourCode: 1, remarks: 'Wow, I am SO impressed. My work here is done.'},
+        { date: '03-01-2017', colourCode: 2, remarks: '...Or not, whatevs.'}
+      ]},
+      { firstName: 'Garner', lastName: 'Kinkle', imageURL: 'https://goo.gl/DPyPYh', assessments: [
+        { date: '02-01-2017', colourCode: 3, remarks: 'I wish that salt bae guy would come by and just sprinkle some on this person.'},
+        { date: '03-01-2017', colourCode: 3, remarks: 'THE STRUGGLE IS REAL'}
+      ]},
+      { firstName: 'Barko', lastName: 'Wilk', imageURL: 'https://i.imgur.com/NUyttbnh.jpg', assessments: [
+        { date: '02-01-2017', colourCode: 3, remarks: 'Keep an eye on this one!'},
+        { date: '03-01-2017', colourCode: 1, remarks: 'Wow, I am SO impressed. My work here is done.'}
+      ]},
+      { firstName: 'Smeini', lastName: 'Luft', imageURL: 'https://i.imgur.com/NUyttbnh.jpg', assessments: [
+        { date: '02-01-2017', colourCode: 1, remarks: 'Wow, I am SO impressed. My work here is done.'},
+        { date: '03-01-2017', colourCode: 1, remarks: 'Knows all the words to Rappers Delight and not afraid to prove it.'}
       ]}
     ]
   }
